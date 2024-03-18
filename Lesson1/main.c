a38b3288a298316d37642593d9192a308f4fcef4
Nikolay Stepankov <gmpcrm@gmail.com>
2024-03-18 15:51:29 +0200

    Lesson 1

---
+++ Lesson1/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+	// Задание 1: Вывести на экран “Hello world!”
+	printf("Hello world!\n\n");
+
+	// Задание 2: Вывести на экран текст "лесенкой"
+	printf("Let's\n");
+	printf("   go\n");
+	printf("    to walk\n\n");
+
+	// Задание 3: Вывести на экран рисунок из букв
+	printf("  *\n");
+	printf(" ***\n");
+	printf("*****\n");
+	printf("*******\n");
+	printf(" HH HH\n");
+	printf(" ZZZZZ\n\n");
+
+	// Задание 4*: Нарисовать соты
+	printf(" __    __\n");
+	printf("/  \\__/  \\\n");
+	printf("\\__/  \\__/\n");
+	printf("/  \\__/  \\\n");
+	printf("\\__/  \\__/\n");
+
+	return 0;
+}
